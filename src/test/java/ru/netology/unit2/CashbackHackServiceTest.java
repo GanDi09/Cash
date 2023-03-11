3ba24dbd453321df30788dd129470029309b7d50
Rinal Shaihutdinov <iversonmelo.6@yandex.ru>
2023-03-11 15:08:17 +0300

    testng commit

--- src/test/java/ru/netology/unit2/CashbackHackServiceTest.java
+++ src/test/java/ru/netology/unit2/CashbackHackServiceTest.java
@@ -7,6 +7,38 @@ import static org.testng.Assert.*;
 public class CashbackHackServiceTest {
 
     @Test
+    public void testRemainBorder() {
+        CashbackHackService service = new CashbackHackService();
+
+        int amount = 900;
+
+        int actual = service.remain(amount);
+        int expected = 100;
+
+        assertEquals(actual, expected);
+    }
+
+    @Test
+    public void testRemainUpBorder() {
+        CashbackHackService service = new CashbackHackService();
+
+        int amount = 1100;
+
+        int actual = service.remain(amount);
+        int expected = 0;
+
+        assertEquals(actual, expected);
+    }
+
+    @Test
     public void testRemain() {
+        CashbackHackService service = new CashbackHackService();
+
+        int amount = 0;
+
+        int actual = service.remain(amount);
+        int expected = 1000;
+
+        assertEquals(actual, expected);
     }
 }
