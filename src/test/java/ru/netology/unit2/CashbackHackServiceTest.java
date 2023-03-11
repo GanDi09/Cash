d7a1dd19f42c34fa87ab01d9916215be51fd2abc
Rinal Shaihutdinov <iversonmelo.6@yandex.ru>
2023-03-11 13:38:47 +0300

    test commit

---
+++ src/test/java/ru/netology/unit2/CashbackHackServiceTest.java
@@ -0,0 +1,12 @@
+package ru.netology.unit2;
+
+import org.testng.annotations.Test;
+
+import static org.testng.Assert.*;
+
+public class CashbackHackServiceTest {
+
+    @Test
+    public void testRemain() {
+    }
+}
